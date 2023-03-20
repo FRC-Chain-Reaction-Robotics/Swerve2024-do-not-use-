e970cdbdc6885128601270f42662ea341e4723cb
HarishKaushik15 <harishkr424@gmail.com>
2023-03-20 17:16:20 -0500

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -74,7 +74,7 @@ public class RobotContainer {
     // .whileFalse(new RunCommand(() -> m_arm.move(0), m_arm));
     
     //m_operatorController.a().onTrue(m_arm.extended() ? new RetractArm(m_arm) : new ExtendArm(m_arm, 0));
-    
+    m_driverController.leftBumper().onTrue(new InstantCommand(() -> m_swerve.slowMode(), m_swerve));
     
   }
 
